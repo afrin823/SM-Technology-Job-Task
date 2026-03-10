af02ec364ea12fcd024b8d18dde6e87ac14a3154
afrin823 <afrin12490@gmail.com>
2026-03-10 22:07:32 +0600

    Let’s Scale Your Business section all compelete

--- script.js
+++ script.js
@@ -4,3 +4,68 @@ const menu = document.getElementById("menu");
 hamburger.addEventListener("click", ()=> {
     menu.classList.toggle("active")
 })
+
+
+
+const monthYear = document.getElementById("monthYear")
+const datesContainer = document.getElementById("dates")
+
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+
+let date = new Date()
+let today = new Date().getDate()
+
+function renderCalendar(){
+
+const year = date.getFullYear()
+const month = date.getMonth()
+
+const firstDay = new Date(year, month, 1).getDay()
+const lastDate = new Date(year, month+1, 0).getDate()
+
+datesContainer.innerHTML=""
+
+monthYear.innerText =
+date.toLocaleString("default",{month:"long"}) + " " + year
+
+for(let i=1;i<firstDay;i++){
+const blank=document.createElement("div")
+datesContainer.appendChild(blank)
+}
+
+for(let i=1;i<=lastDate;i++){
+
+const day=document.createElement("div")
+
+day.innerText=i
+
+// DEFAULT ACTIVE DATE
+if(i === today){
+day.classList.add("active")
+}
+
+day.onclick=()=>{
+document.querySelectorAll(".dates div")
+.forEach(d=>d.classList.remove("active"))
+
+day.classList.add("active")
+}
+
+datesContainer.appendChild(day)
+
+}
+
+}
+
+prev.onclick=()=>{
+date.setMonth(date.getMonth()-1)
+renderCalendar()
+}
+
+next.onclick=()=>{
+date.setMonth(date.getMonth()+1)
+renderCalendar()
+}
+
+renderCalendar()
